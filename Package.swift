2161dd75bc9c49b947dcb59047cd3a113438e72c
Nick Lane-Smith <nick@cardscan.ai>
2024-12-03 19:30:47 -0800

    adding cardscan client dependency

--- Package.swift
+++ Package.swift
@@ -6,16 +6,22 @@ import PackageDescription
 let package = Package(
     name: "InsuranceCardScan",
     platforms: [
-      .iOS(.v15)
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "InsuranceCardScan",
-            targets: ["InsuranceCardScan"]),
+            targets: ["InsuranceCardScan"]
+        ),
+    ],
+    dependencies: [
+        // Adding a new dependency
+        .package(url: "https://github.com/CardScan-ai/api-clients", from: "0.9.3"),
     ],
     targets: [
         .binaryTarget(
-          name: "InsuranceCardScan",
-          path: "./Sources/InsuranceCardScan.xcframework")
+            name: "InsuranceCardScan",
+            path: "./Sources/InsuranceCardScan.xcframework"
+        )
     ]
 )
